af063341b9e4b304b583321790271085d07131f2
hariskh842 <117294367+hariskh842@users.noreply.github.com>
2025-12-01 22:06:10 -0500

    Update hbm_access_helpers.cpp

--- src/hbm_access_helpers.cpp
+++ src/hbm_access_helpers.cpp
@@ -1,72 +1,82 @@
 #include "hbm_access_helpers.h"
 
-// Helper functions for efficient HBM access
-void cache_find(
-    ap_uint<128> *hbm_clauseStore,
-    ap_uint<128> *mClsStore,
-    ClsCacheData &clsCacheData,
-    unsigned int addr,
-    unsigned int &found_addr,
-    bool &hit)
-{
-    #pragma HLS inline off
+// Configuration for set-associative cache
+#define NUM_WAYS 4  // Number of ways (associativity)
+#define NUM_SETS (CACHE_SIZE / NUM_WAYS)  // Number of sets
 
-    LOOKUP_LOOP:
-    for (int i = 0; i < CACHE_SIZE; i++) {
-	// parallelizes the lookup
-        // #pragma HLS unroll
-        if (clsCacheData.cache_bits[i][0] && clsCacheData.cache_tag[i] == addr) {
-	    found_addr = i;
-            hit = true;
-	    break;
-        }
-    }
-}
-
-// Read single clause element from HBM
+// Read single clause element from HBM with set-associative cache
 ap_uint<128> hbm_read_clause_element(
     ap_uint<128> *hbm_clauseStore,
     ap_uint<128> *mClsStore,
     ClsCacheData &clsCacheData,
     unsigned int addr)
 {
-    // fully associative search
-    // cache_find(hbm_clauseStore, mClsStore, clsCacheData, addr, found_addr, hit);
-
-    // direct mapped search
-    unsigned int index = addr % CACHE_SIZE;
-
-    //printf("Reading addr: %h, with cache access %h", addr, index);
-
-    ap_uint<128> get;
-
-    if (clsCacheData.cache_bits[index][0] && clsCacheData.cache_tag[index] == addr) {
-	//printf("Data read: %h", mClsStore[index]);
-        get = mClsStore[index];
-    }
-    else{
-        ap_uint<128> fetched = reg(reg(hbm_clauseStore[addr]));
+    // Extract set index from address
+    unsigned int set_index = addr % NUM_SETS;
+    unsigned int base_index = set_index * NUM_WAYS;
     
-        // writeback dirty
-        if(clsCacheData.cache_bits[index][1]){
-	    //printf("Wrote back addr %h from line %h", clsCacheData.cache_tag[index], index);
-	    //printf("Had value of %h", mClsStore[index]);
-            hbm_clauseStore[clsCacheData.cache_tag[index]] = mClsStore[index];
+    // Search within the set for a hit
+    bool hit = false;
+    unsigned int hit_way = 0;
+    
+    SEARCH_WAYS:
+    for (unsigned int way = 0; way < NUM_WAYS; way++) {
+        #pragma HLS unroll
+        unsigned int cache_index = base_index + way;
+        if (clsCacheData.cache_bits[cache_index][0] && 
+            clsCacheData.cache_tag[cache_index] == addr) {
+            hit = true;
+            hit_way = way;
+            break;
         }
+    }
     
-        mClsStore[index] = fetched;
-        clsCacheData.cache_tag[index]   = addr;
-        clsCacheData.cache_bits[index][0] = 1; // valid
-        clsCacheData.cache_bits[index][1] = 0; // dirty
+    ap_uint<128> get;
     
-        //printf("Data read: %h", fetched);
+    if (hit) {
+        // Cache hit
+        unsigned int cache_index = base_index + hit_way;
+        get = mClsStore[cache_index];
+    }
+    else {
+        // Cache miss - fetch from HBM
+        ap_uint<128> fetched = reg(reg(hbm_clauseStore[addr]));
+        
+        // Find replacement candidate (simple: first invalid or use way 0)
+        unsigned int replace_way = 0;
+        bool found_invalid = false;
+        
+        FIND_REPLACEMENT:
+        for (unsigned int way = 0; way < NUM_WAYS; way++) {
+            #pragma HLS unroll
+            unsigned int cache_index = base_index + way;
+            if (!clsCacheData.cache_bits[cache_index][0]) {
+                replace_way = way;
+                found_invalid = true;
+                break;
+            }
+        }
+        
+        unsigned int victim_index = base_index + replace_way;
+        
+        // Writeback dirty line if necessary
+        if (clsCacheData.cache_bits[victim_index][1]) {
+            hbm_clauseStore[clsCacheData.cache_tag[victim_index]] = mClsStore[victim_index];
+        }
+        
+        // Install new line in cache
+        mClsStore[victim_index] = fetched;
+        clsCacheData.cache_tag[victim_index] = addr;
+        clsCacheData.cache_bits[victim_index][0] = 1; // valid
+        clsCacheData.cache_bits[victim_index][1] = 0; // dirty
+        
         get = fetched;
     }
+    
     return get;
 }
 
-
-// Write single clause element to HBM
+// Write single clause element to HBM with set-associative cache
 void hbm_write_clause_element(
     ap_uint<128> *hbm_clauseStore,
     ap_uint<128> *mClsStore,
@@ -74,34 +84,61 @@ void hbm_write_clause_element(
     unsigned int addr,
     ap_uint<128> data)
 {
-    bool hit = false;
-
-    // fully associative
-    // cache_find(hbm_clauseStore, mClsStore, clsCacheData, addr, found_addr, hit);
-
-    // direct mapped search
-    unsigned int index = addr % CACHE_SIZE;
-    //printf("Writing addr: %h, with cache access %h", addr, index);
-    //printf("Data written: %h", data);
-
-    if (clsCacheData.cache_bits[index][0] && clsCacheData.cache_tag[index] == addr) {
-        // Cache hit: update cache, mark dirty
-        mClsStore[index] = data;
-        clsCacheData.cache_bits[index][1] = 1;
-    }
-    else{
-        if (clsCacheData.cache_bits[index][1]) {
-            // writeback dirty line to HBM
-	    //printf("Wrote back addr %h from line %h", clsCacheData.cache_tag[index], index);
-	    //printf("Had value of %h", mClsStore[index]);
-            hbm_clauseStore[clsCacheData.cache_tag[index]] = mClsStore[index];
-        }
+    // Extract set index from address
+    unsigned int set_index = addr % NUM_SETS;
+    unsigned int base_index = set_index * NUM_WAYS;
     
-        // replace data in cache
-        mClsStore[index] = data;
-        clsCacheData.cache_tag[index] = addr;
-        clsCacheData.cache_bits[index][0] = 1; // valid
-        clsCacheData.cache_bits[index][1] = 1; // dirty
+    // Search within the set for a hit
+    bool hit = false;
+    unsigned int hit_way = 0;
+    
+    SEARCH_WAYS_WRITE:
+    for (unsigned int way = 0; way < NUM_WAYS; way++) {
+        #pragma HLS unroll
+        unsigned int cache_index = base_index + way;
+        if (clsCacheData.cache_bits[cache_index][0] && 
+            clsCacheData.cache_tag[cache_index] == addr) {
+            hit = true;
+            hit_way = way;
+            break;
+        }
+    }
+    
+    if (hit) {
+        // Cache hit: update cache, mark dirty
+        unsigned int cache_index = base_index + hit_way;
+        mClsStore[cache_index] = data;
+        clsCacheData.cache_bits[cache_index][1] = 1;
+    }
+    else {
+        // Cache miss - need to allocate line
+        
+        // Find replacement candidate (simple: first invalid or use way 0)
+        unsigned int replace_way = 0;
+        bool found_invalid = false;
+        
+        FIND_REPLACEMENT_WRITE:
+        for (unsigned int way = 0; way < NUM_WAYS; way++) {
+            #pragma HLS unroll
+            unsigned int cache_index = base_index + way;
+            if (!clsCacheData.cache_bits[cache_index][0]) {
+                replace_way = way;
+                found_invalid = true;
+                break;
+            }
+        }
+        
+        unsigned int victim_index = base_index + replace_way;
+        
+        // Writeback dirty line if necessary
+        if (clsCacheData.cache_bits[victim_index][1]) {
+            hbm_clauseStore[clsCacheData.cache_tag[victim_index]] = mClsStore[victim_index];
+        }
+        
+        // Install new line in cache and mark dirty
+        mClsStore[victim_index] = data;
+        clsCacheData.cache_tag[victim_index] = addr;
+        clsCacheData.cache_bits[victim_index][0] = 1; // valid
+        clsCacheData.cache_bits[victim_index][1] = 1; // dirty
     }
 }
-
